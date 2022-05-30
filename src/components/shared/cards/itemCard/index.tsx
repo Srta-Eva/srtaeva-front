f3c4e21ef559b53bd68ddda0d4b53be4523b2251
Jaime <burbanojaime98@gmail.com>
2022-05-30 13:22:40 -0500

    Some responsive issues fixed

--- src/components/shared/cards/itemCard/index.tsx
+++ src/components/shared/cards/itemCard/index.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { Box, Button, Divider, Image, Stat, StatNumber, Text } from "@chakra-ui/react";
+import React from 'react';
+import { Box, Button, Divider, Image, Stat, StatNumber, Text } from '@chakra-ui/react';
 import css from './index.module.scss';
-import { ThousandSeparator } from "@utils";
-import {ButtonGroup} from "@components";
-import { Link } from "react-router-dom";
+import { ThousandSeparator } from '@utils';
+import { ButtonGroup } from '@components';
+import { Link } from 'react-router-dom';
 
 interface ItemCardProps {
   name: string;
@@ -13,66 +13,60 @@ interface ItemCardProps {
   specialPrice?: number;
 }
 
-const ItemCard:React.FC<ItemCardProps> = (props) => {
+const ItemCard: React.FC<ItemCardProps> = props => {
   const { name, price, image, specialPrice } = props;
 
   return (
     <Box
       className={css['root']}
-      borderRadius="lg"
-      border='1px' borderColor='gray.200'
+      borderRadius='lg'
+      border='1px'
+      borderColor='gray.200'
       boxShadow='md'
     >
       <Box className={css['root-content']}>
-        <Box as={Link} to="#" className={css['root-img']}>
+        <Box as={Link} to='#' className={css['root-img']}>
           <Image
             className={css['root-img-content']}
             src={image}
             fallbackSrc='https://via.placeholder.com/150'
-            alt="product"
+            alt='product'
           />
         </Box>
-        <Box as="main" className={css['root-body']}>
-          <Link to="#">
-            <Text
-              fontSize="lg"
-              className={css['root-title']}
-            >
+        <Box as='main' className={css['root-body']}>
+          <Link to='#'>
+            <Text fontSize='md' className={css['root-title']}>
               {name}
             </Text>
-            <Box className={css['root-stars']}>
-              ⭐⭐⭐⭐⭐
-            </Box>
+            <Box className={css['root-stars']}>⭐⭐⭐⭐⭐</Box>
             <Stat className={css['root-price']}>
-              <StatNumber>${ThousandSeparator(price)}</StatNumber>
+              <StatNumber fontSize='md'>${ThousandSeparator(price)}</StatNumber>
             </Stat>
 
-            {specialPrice && <Stat>
-              <StatNumber>${ThousandSeparator(specialPrice)}</StatNumber>
-            </Stat>}
+            {specialPrice && (
+              <Stat className={css['root-price']}>
+                <StatNumber fontSize='md'>${ThousandSeparator(specialPrice)}</StatNumber>
+              </Stat>
+            )}
           </Link>
 
-          <Divider className={css['root-divider']}/>
+          <Divider className={css['root-divider']} />
           <Box className={css['root-btnGroup']}>
             <ButtonGroup
               items={[
-                {name: '2kg', disabled: false},
-                {name: '4kg', disabled: false},
-                {name: '5kg', disabled: true},
+                { name: '2kg', disabled: false },
+                { name: '4kg', disabled: false },
+                { name: '5kg', disabled: true },
               ]}
             />
           </Box>
         </Box>
       </Box>
-      <Button
-        display="block"
-        className={css['root-addBtn']}
-        colorScheme='teal'
-      >
+      <Button display='block' className={css['root-addBtn']} colorScheme='teal'>
         Agregar al carrito
       </Button>
     </Box>
-  )
-}
+  );
+};
 
-export default ItemCard
+export default ItemCard;
--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,44 +1,59 @@
 import React from 'react';
-import { Button, Center, Text, IconButton, Box } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  Text,
+  IconButton,
+  Box,
+  Fade,
+  useMediaQuery,
+  ScaleFade,
+} from '@chakra-ui/react';
 import { NavLink, Link as ReactLink } from 'react-router-dom';
 import { HOME_PATH, PRODUCTS_PATH, SERVICES_PATH, US_PATH } from '@routes';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import css from './index.module.scss';
 
 const Header: React.FC = () => {
+  const [isLargerThan428] = useMediaQuery('(min-width: 428px)');
+  const [isLargerThan668] = useMediaQuery('(min-width: 668px)');
+
   return (
-    <Box as='header' className={css['header']} borderColor='gray.200' bg='white' zIndex="sticky">
+    <Box as='header' className={css['header']} borderColor='gray.200' bg='white' zIndex='sticky'>
       <Box className={css['header-box']}>
         <Box className={css['header-boxLogo']}>
-          <Box className={css['header-menuBtn']}>
+          <ScaleFade in={!isLargerThan668} unmountOnExit>
             <IconButton size='sm' aria-label='Toggle menu' icon={<HamburgerIcon />} />
-          </Box>
+          </ScaleFade>
           <ReactLink to={HOME_PATH}> Srta. Eva </ReactLink>
         </Box>
         <Box as='nav' className={css['header-nav']}>
-          <NavLink
-            to={US_PATH}
-            className={({ isActive}) => isActive ? css['header-nav-item-active'] : ''}
-          >
-            Nosotros
-          </NavLink>
-          <NavLink
-            to={PRODUCTS_PATH}
-            className={({ isActive}) => isActive ? css['header-nav-item-active'] : ''}
-          >
-            Productos
-          </NavLink>
-          <NavLink
-            to={SERVICES_PATH}
-            className={({ isActive}) => isActive ? css['header-nav-item-active'] : ''}
-          >
-            Servicios
-          </NavLink>
+          <Fade in={isLargerThan668} unmountOnExit>
+            <NavLink
+              to={US_PATH}
+              className={({ isActive }) => (isActive ? css['header-nav-item-active'] : '')}
+            >
+              Nosotros
+            </NavLink>
+            <NavLink
+              to={PRODUCTS_PATH}
+              className={({ isActive }) => (isActive ? css['header-nav-item-active'] : '')}
+            >
+              Productos
+            </NavLink>
+            <NavLink
+              to={SERVICES_PATH}
+              className={({ isActive }) => (isActive ? css['header-nav-item-active'] : '')}
+            >
+              Servicios
+            </NavLink>
+          </Fade>
         </Box>
+
         <Box className={css['header-boxBtns']}>
-          <Box className={css['header-loginBtn']}>
+          <ScaleFade in={isLargerThan428} unmountOnExit>
             <Button size='sm'>Iniciar sesión</Button>
-          </Box>
+          </ScaleFade>
           <Center
             as={Button}
             position='relative'
